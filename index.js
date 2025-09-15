28dd3467136a61ee05854feb6bcb463c782e3042
vipul kumar <bca23161@glbim.ac.in>
2025-09-15 10:00:20 +0530

    wallet creation DONE

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const bip39 = require("bip39");
-const {BIP32Factory} = require("bip32");
+const { BIP32Factory } = require("bip32");
 const bitcoin = require("bitcoinjs-lib");
 const ethers = require("ethers");
 const ecc = require("tiny-secp256k1");
-const {Keypair } = require("@solana/web3.js");
-const {derivePath} = require('ed25519-hd-key')
-const bs58 = require('bs58');
+const { Keypair } = require("@solana/web3.js");
+const { derivePath } = require("ed25519-hd-key");
+const bs58 = require("bs58").default;
 
+const bip32 = BIP32Factory(ecc);
 
 function deriveBitcoinWallet(seed) {
   const btcPath = "m/44'/0'/0'/0/0";
 
   const rootNode = bip32.fromSeed(seed);
   const btcNode = rootNode.derivePath(btcPath);
-  const btcAddress = bitcoin.payment.p2pkh({
+
+  const btcAddress = bitcoin.payments.p2pkh({
     pubkey: Buffer.from(btcNode.publicKey),
   }).address;
 
@@ -27,6 +29,31 @@ function deriveBitcoinWallet(seed) {
   console.log("address", btcAddress);
 }
 
+function deriveEthereumWallet(seed) {
+  const ethPath = "m/44'/60'/0'/0/0";
+
+  const rootNode = ethers.HDNodeWallet.fromSeed(seed);
+  const ethNode = rootNode.derivePath(ethPath);
+
+  console.log("derivation path ", ethPath);
+  console.log("private key", ethNode.privateKey);
+  console.log("public key", ethNode.publicKey);
+  console.log("address", ethNode.address);
+}
+
+function deriveSolanaWallet(seed) {
+  const solanaPath = "m/44'/501'/0'/0'";
+  const solanaDerivedSeed = derivePath(solanaPath, seed).key;
+  const solanaKeyPair = Keypair.fromSeed(solanaDerivedSeed);
+
+  const solanaAddress = solanaKeyPair.publicKey.toBase58();
+  const solanaPrivateKey = bs58.encode(solanaKeyPair.secretKey);
+
+  console.log("Derivation Path", solanaPath);
+  console.log("PrivateKey", solanaPrivateKey);
+  console.log("public key/address", solanaAddress);
+}
+
 async function main() {
   const mnemonic = bip39.generateMnemonic();
 
@@ -36,6 +63,11 @@ async function main() {
 
   deriveBitcoinWallet(seed);
   deriveEthereumWallet(seed);
+  deriveSolanaWallet(seed);
 
   console.log("wallet generation successful");
 }
+
+main().catch((err) => {
+  console.error(err);
+});
